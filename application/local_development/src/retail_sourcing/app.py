1da2fe04c6b76f5e4ad8603cf75b958e112a86e5
Dan Collins <dan2collins@hotmail.co.uk>
2024-06-11 17:52:14 +0000

    Updated the application for ETL to perform DQ checks with cuallee, as a starter looking at customers

--- application/local_development/src/retail_sourcing/app.py
+++ application/local_development/src/retail_sourcing/app.py
@@ -11,6 +11,7 @@ import os
 import sys 
 import logging 
 import yaml 
+from cuallee import Check, CheckLevel 
 from datetime import date, timedelta
 import pandas as pd 
 import duckdb as ddb 
@@ -79,6 +80,7 @@ def merge_customers_postcodes(cust_df: pd.DataFrame, postcode_df: pd.DataFrame) 
         ddb.query(
         """
         SELECT 
+            CURRENT_DATE() AS EXTRACT_DATE,
             TB1.CUSTOMERID, TB1.FIRSTNAME, TB1.LASTNAME, TB1.REWARDSMEMBER, TB1.EMAILADDRESS, 
             TB1.PROFESSION, TB1.DOB, TB1.CUSTOMERJOINED, 
             TB1.POSTCODE,
@@ -105,12 +107,10 @@ def write_to_snowflake(snowpark_df: object, snowflake_schema: str, snowflake_tab
         snowflake_table (str): tabke to target
     """
     snowpark_df.write.mode(mode).save_as_table(f"{snowflake_schema}.{snowflake_table}")
-    
 
 
 # run the ETL application 
 if __name__ == "__main__":
-    #pd.set_option('display.max_colwidth', None)
     # configure logging 
     log = logger(output='terminal', level='INFO', filename=None) # use defaults 
     log.info("=" * 150) 
@@ -137,8 +137,29 @@ if __name__ == "__main__":
         customers_df = get_customers(seed=87654) # Leave this seed hardcoded, so customers are always the same for this example project
         log.info(f"Customer data sourced. Result dataframe has {len(customers_df)} rows")
 
-        # DQ
-        # do some data quality here 
+        # DQ : do some data quality here with cuallee (on top of Pandas)
+        log.info("Running DQ checks on Customers Extract ...")
+        cust_checks = Check(level=CheckLevel.ERROR, name="customer_extract_DQ")
+        cust_dq = (cust_checks
+            .are_complete(["customerID", "emailAddress", "firstName", "lastName", "profession"])
+            .is_unique("customerID")
+            .is_positive("customerID")
+            .validate(customers_df) # choose the DataFrame to validate the checks against 
+        )
+        log.info(f"\n{cust_dq.to_string()}\n") # logs the results of DQ checks
+        # now run a check to see if any FAILS exist & raise if so 
+        cust_dq_fails = (
+            ddb.query("""
+            SELECT 
+                SUM(CASE WHEN STATUS = 'FAIL' THEN 1 ELSE 0 END) AS DQ_FAILURES
+            FROM cust_dq 
+            """).to_df().iloc[0, 0] # accesses the actual value 
+        )
+        if cust_dq_fails != 0:
+            log.error("There has been a DQ failure for the extraction of customer data")
+            raise Exception("DQ Failure for Customer Extract")
+        else:
+            log.info("DQ checks on Customers Extract have passed!")
 
         # postcodes
         log.info("Source UK Postcode data from API extraction ...") 
@@ -154,6 +175,17 @@ if __name__ == "__main__":
         tomorrow = (date.today() + timedelta(days=1)) 
         txns_df = get_transactions(today, tomorrow)
         log.info(f"Transactions data sourced. Result dataframe has {len(txns_df)} rows")
+        # use DuckDB to add extract date 
+        txns_df_w_extract = (
+            ddb.query(
+                """
+                SELECT 
+                    *,
+                    CURRENT_DATE() AS EXTRACT_DATE
+                FROM txns_df 
+                """
+            ).to_df() # converts result back to dataframe 
+        )
 
         # DQ
         # do some data quality here
@@ -187,8 +219,8 @@ if __name__ == "__main__":
         log.info("data written to snowflake") 
 
         # Now write transactions data to snowpark & snowflake 
-        log.info("push txns_df to snowpark")
-        txns_snow = snowpark.create_dataframe(txns_df) 
+        log.info("push txns_df_w_extract to snowpark")
+        txns_snow = snowpark.create_dataframe(txns_df_w_extract) 
         log.info("write txns_df to snowflake") 
         write_to_snowflake(txns_snow, "PURCHASE_DATA_STG", "RAW_TXN_DATA")
         log.info("data written to snowflake\n")
